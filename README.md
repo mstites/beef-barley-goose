ca54157dce6a44438cacc9ee293c2d9ebbe109f9
mstites <maeve@mstites.com>
2022-07-23 04:00:24 -0400

    Sketch out potential classes

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # platformer
 Maybe I will make a game or something (?)
+
+## Target MVP
+
+## Stretch Goals
+* 3 player game
+
+
+# Notes
+## To Do: Short Term Goals
+* Create place holder beef, barley, goose jpg images
+## To Do: Future Tasks
+* Convert vector images to bitmap on startup
+## Gameplay
+Characters:
+* Beef - human character
+* Barley - cute little Barley with face
+* Goose - 
+
+General mechanics (possibilities):
+1. Beef as main character with Barley as a support char that gives bonuses, health packs, etc. Goose is a support char that can honk (with cool down) to scare away certain enemies, or maybe some sort of earth quake.
+2. You unlock each character as you progress. Assemble a team to accomplish some goal, defeat bosses, etc. Two thoughts on possibilities:
+    a.  Can rotate through the characters in game (such as pokemon trainer in smash). 
+    b. Select before room.
+3. Could have a center room / base from which to launch missions. With a map with position marker etc.
---
+++ actors.py
@@ -0,0 +1,61 @@
+class Entity():
+    """Base entity qualities and abilities"""
+    def __init__(
+        self, 
+        position=(0, 0), 
+        speed=2,
+        img="assets/img/vector/default"):
+        self.position=position
+        self.speed=speed
+
+        # TURN VECTOR IMAGE INTO BITMAP AND APPLY
+        self.health
+        self.armor
+        self.
+
+    def jump():
+        # jump
+        return
+
+    def strafe():
+        # move side to side
+        return
+    
+    def remove():
+        # remove from screen
+        return
+
+class Mob(Entity):
+    def crouch():
+        # crouch
+        return
+
+########################
+###### Players ######
+########################
+class Player():
+class Beef():
+    # human char
+    def __init__():
+        return
+class Barley():
+    # human char
+    def __init__():
+        return
+class Goose():
+    def __init__():
+        return
+
+########################
+######## Bosses ########
+########################
+# Horton, 
+class boss1():
+    # bosses here, give clever name
+    def __init__():
+        return
+
+########################
+###### Characters ######
+########################
+# in game characters / non combat / challenge
